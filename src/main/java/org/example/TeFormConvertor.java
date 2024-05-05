fe91073e9dc4f7694acae3e79890ba9fa2aeaa3f
Igor Shcherbakov <scherbakovigor@icloud.com>
2024-05-05 21:49:22 +0300

    Cleanup

--- src/main/java/org/example/TeFormConvertor.java
+++ src/main/java/org/example/TeFormConvertor.java
@@ -1,38 +1,37 @@
 package org.example;
 
 public class TeFormConvertor {
-    public static String convert(String verb){
-        if (ConjugationDefiner.defineConjugation(verb).equals(Conjugation.THIRD)){
-            switch (verb){
-                case "する" : return "して";
-                case "くる" : return "きて";
-            }
+    public static String convert(String verb) {
+        Conjugation conjugation = ConjugationDefiner.defineConjugation(verb);
+
+        if (conjugation == Conjugation.THIRD) {
+            return switch (verb) {
+                case "する" -> "して";
+                case "くる" -> "きて";
+                default -> throw new IllegalStateException("Unexpected value: " + verb);
+            };
         }
 
-        if (ConjugationDefiner.defineConjugation(verb).equals(Conjugation.SECOND)){
-            return verb.substring(0, verb.length() - 1 ).concat("て");
+        String withoutLastSymbol = verb.substring(0, verb.length() - 1);
+        if (conjugation == Conjugation.SECOND) {
+            return withoutLastSymbol.concat("て");
         }
-// って　　
-        if (ConjugationDefiner.defineConjugation(verb).equals(Conjugation.FIRST)){
-            if (verb.equals("いく")) { return "いって"; }
+
+        if (conjugation == Conjugation.FIRST) {
+            if (verb.equals("いく")) {
+                return "いって";
+            }
 
             char lastSymbol = verb.charAt(verb.length() - 1);
 
-            if (lastSymbol == 'う' || lastSymbol == 'つ' || lastSymbol == 'る' ){
-                return verb.substring(0, verb.length() - 1 ).concat("って");
-            }
-            if (lastSymbol == 'ぬ' || lastSymbol == 'ぶ' || lastSymbol == 'む'){
-                return verb.substring(0, verb.length() - 1 ).concat("んで");
-            }
-            if (lastSymbol == 'く'){
-                return verb.substring(0, verb.length() - 1 ).concat("いて");
-            }
-            if (lastSymbol == 'ぐ'){
-                return verb.substring(0, verb.length() - 1 ).concat("いで");
-            }
-            if (lastSymbol == 'す'){
-                return verb.substring(0, verb.length() - 1 ).concat("して");
-            }
+            return withoutLastSymbol + switch (lastSymbol) {
+                case 'う', 'つ', 'る' -> "って";
+                case 'ぬ', 'ぶ', 'む' -> "んで";
+                case 'く' -> "いて";
+                case 'ぐ' -> "いで";
+                case 'す' -> "して";
+                default -> throw new IllegalStateException("Unexpected value: " + lastSymbol);
+            };
         }
         return verb;
     }
